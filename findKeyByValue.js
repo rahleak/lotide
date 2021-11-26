a075d7ea1a4ee6228013568f981262fc86f26044
rahleak <rah.leak@gmail.com>
2021-11-26 01:39:42 +0000

    best tv shows

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Asserstion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function (obj, val){
+
+  for (const i in obj) {
+    if (obj[i] === val) {
+      return i
+    }
+  }
+}
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
