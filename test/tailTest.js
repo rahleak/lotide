64398a263aec5384007a8b49910ebde8b6b33211
rahleak <rah.leak@gmail.com>
2021-11-30 23:40:57 +0000

    refactored with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,16 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../head');
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['6']), '5'); 
+  })
+});
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); 
